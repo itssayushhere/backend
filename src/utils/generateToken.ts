192bcfd162214b8a7f796f3d057002a1f6477870
novo-deployment <novoaccounts@enpointe.io>
2025-12-02 22:19:40 +0530

    add !

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,4 +1,4 @@
 import jwt from 'jsonwebtoken';
 export const generateToken = (data : object) => {
-  return jwt.sign({ ...data }, process.env.JWT_SECRET, { expiresIn: '30d' });
+  return jwt.sign({ ...data }, process.env.JWT_SECRET!, { expiresIn: '30d' });
 };
